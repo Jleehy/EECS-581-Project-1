97da83b1369061e2ce90736f62773415cfb1e0f7
seanhammell <sean.m.hammell@gmail.com>
2024-09-13 17:15:06 -0500

    Implement basic attack loop
    Creates a basic attack loop with appropriate cursor clearing to hide
    information from enemy players.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     # Create each player's board.
     player1 = Board("Player 1")
     player2 = Board("Player 2")
+    players = [player1, player2]
     
     # Create the Battleship app.
     app = App(player1, player2)
@@ -16,8 +17,15 @@ def main():
     app.prompt_num_ships()
 
     # Give each player a chance to place their ships.
-    for player in [player1, player2]:
-        print("\n" + player.name + "'s turn to place their ships")
+    for player in players:
+        cursor.erase()
+        action = input(f"\n{player.name}, press Enter to begin placing your ships...")
+        app.check_quit(action)
+
+        cursor.move_up(1)
+        cursor.erase()
+
+        print(player.name + "'s turn to place their ships")
 
         # For each ship in the player's arsenal.
         for ship in range(app.num_ships):
@@ -47,7 +55,7 @@ def main():
         app.print_board(player) # Ensure board prints on last turn.
 
         # Ask the player if they are ready to turn the device over to the second player.
-        action = input(f"{player.name}, are you ready to turn the device over to the next player? Press Enter to continue...")
+        action = input(f"{player.name}, press Enter after reviewing your ship placements...")
         app.check_quit(action)
 
         # Move the cursor to the line after "Enter the number of ships" to
@@ -55,33 +63,65 @@ def main():
         cursor.move_to(2)
 
     # Begin the game loop.
+    current_player = 0
     while True:
+        cursor.move_to(0)
+        cursor.erase()
+
+        print(players[current_player].name + "'s turn to attack\n")
+        action = input(f"{players[current_player].name}, press Enter to begin attacking...")
+        app.check_quit(action)
+
+        cursor.move_up(1)
+        cursor.erase()
+
+        print("Your Board")
+        app.print_board(players[current_player])
+        print("Enemy's Board")
+        app.print_board(players[current_player], censored = True)
+
+        coord = app.prompt_attack_coordinate()
+        hit, sink = app.attack(players[abs(current_player - 1)], coord)
+
+        if hit or sink:
+            status = "Hit" if hit else "Sunk"
+            print(f"\n{status} {players[abs(current_player - 1)]}'s ship!")
+            continue
+
+        print("\nMissed...")
+
+        # Ask the player if they are ready to turn the device over to the second player.
+        action = input(f"\n{players[current_player].name}, press Enter when you're done reviewing you attacks...")
+        app.check_quit(action)
+
+        current_player = abs(current_player - 1)
+
         # NOTE: Generic prints for now. I just want to debug attack. Currently using player1 and player2 instead of a variable that grabs current player.
         # this isnt a game loop yet its just for debug purposes.
 
-        print("Player 1's turn")
-        print("Your Board")
-        app.print_board(player1)
-        print("Enemies Board")
-        app.print_board(player2, censored=True)
+        # print("Player 1's turn")
+        # print("Your Board")
+        # app.print_board(player1)
+        # print("Enemies Board")
+        # app.print_board(player2, censored=True)
 
-        pos = app.prompt_attack_coordinate()
-        hit, sink = app.attack(player2, pos)
+        # pos = app.prompt_attack_coordinate()
+        # hit, sink = app.attack(player2, pos)
 
-        if not hit:
-            print("MISSED!")
-        if hit:
-            print(f"HIT PLAYER 2 @ {pos}")
-        if sink:
-            print("SUNK PLAYERS SHIP OF SIZE X")
+        # if not hit:
+        #     print("MISSED!")
+        # if hit:
+        #     print(f"HIT PLAYER 2 @ {pos}")
+        # if sink:
+        #     print("SUNK PLAYERS SHIP OF SIZE X")
 
-        print("Player 2's turn")
-        print("Your Board")
-        app.print_board(player2)
-        print("Enemies Board")
-        app.print_board(player1, censored=True)
+        # print("Player 2's turn")
+        # print("Your Board")
+        # app.print_board(player2)
+        # print("Enemies Board")
+        # app.print_board(player1, censored=True)
 
-        break
+        # break
 
 
 if __name__ == "__main__":
