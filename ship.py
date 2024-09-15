74a58460aa2186f2b553f294a97a4355c8727524
Jacob Leehy <117180669+Jleehy@users.noreply.github.com>
2024-09-15 05:27:21 -0500

    added comments

--- ship.py
+++ ship.py
@@ -1,27 +1,42 @@
-class Ship:
+'''
+Program Name: ship,py
+Description: ship.py manages the internal workings of the ships within the game. It is responsible for populating 
+    indices and validating some user input.
+Inputs: None
+Output: None
+Code Sources:
+Authors: Steve Gan, Sean Hammell, Jacob Leehy, Mario Simental, Matthew Sullivan
+Creation Date: 9/9/24
+'''
+class Ship: # class used to represent ships
     #param: length is an int representing the length of the ship
         #start_row is an int representing the y coordinate of the start of the ship
         #start_col is an int representing the x coordinate of the start of the ship
         #vert is a boolean representing if the ship is vertical or not. Assumes that ships only go down or to the right
-    def __init__(self, length, start_row, start_col, vert): 
+    def __init__(self, length, start_row, start_col, vert):  # constructor for ship - takes ship length, start row and col, and vertical bool
         self.indices = set() #indices is a set containing tuples. Each tuple represents one coordinate
-        self._populate_indices(length, start_row, start_col, vert)
+        self._populate_indices(length, start_row, start_col, vert) # calls populate indices
 
-    # Add the indices a ship takes up to a set.
-    def _populate_indices(self, length, start_row, start_col, vert):
-        if vert:
-            for col in range(length):
-                self.indices.add((start_row, start_col + col))  # corrected
-        else:
-            for row in range(length):
-                self.indices.add((start_row + row, start_col))  # corrected
+    '''
+    Defines a method that is used to populate the indices used by the ships. 
+    '''
+    def _populate_indices(self, length, start_row, start_col, vert): # Add the indices a ship takes up to a set.
+        if vert: # if vertical
+            for col in range(length): # iterate over length of ship
+                self.indices.add((start_row, start_col + col))  # add the needed coordinates
+        else: # else not vertical
+            for row in range(length): # iterate over length of ship
+                self.indices.add((start_row + row, start_col))  # add the needed coordinates
     
-    # Check if a ship overlaps another ship.
-    def _is_overlapping(self, otherShip):
-        for indices in otherShip.indices:
-            if indices in self.indices:
-                return True
-        return False
+    '''
+    Defines a method that is used to determine if ships overlap.
+    Returns a boolean indicating the outcome.
+    '''
+    def _is_overlapping(self, otherShip): # Check if a ship overlaps another ship.
+        for indices in otherShip.indices: # iterate over indices in other ship
+            if indices in self.indices: # check if indices is in current ships indices
+                return True # return true
+        return False # return false
     
-    def __repr__(self):
-        return f"Indices: {self.indices}"
+    def __repr__(self): # magic method to help with printing
+        return f"Indices: {self.indices}" # print the indices
