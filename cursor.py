e9c211fef580f72e662158620d2a7ba65f079757
Jacob Leehy <117180669+Jleehy@users.noreply.github.com>
2024-09-15 05:26:37 -0500

    added comments

--- cursor.py
+++ cursor.py
@@ -1,11 +1,17 @@
-# Moves the cursor to the beginning of line n.
-def move_to(n):
-    print(f"\x1B[{n};0H", end = "")
+'''
+Program Name: cursor.py
+Description: cursor.py is responsible for making the game UI user readable. It also obscures important information.
+Inputs: None
+Output: Clears text from the console and moves the cursor.
+Code Sources:
+Authors: Steve Gan, Sean Hammell, Jacob Leehy, Mario Simental, Matthew Sullivan
+Creation Date: 9/9/24
+'''
+def move_to(n): # Moves the cursor to the beginning of line n.
+    print(f"\x1B[{n};0H", end = "") # is used to move the cursor to a specific position in the terminal using ANSI escape codes
 
-# Moves the cursor to the beginnning of the previous line, n lines up.
-def move_up(n):
-    print(f"\x1B[{n}F", end = "")
+def move_up(n): # Moves the cursor to the beginnning of the previous line, n lines up.
+    print(f"\x1B[{n}F", end = "") # is used to move the cursor to a specific position in the terminal using ANSI escape codes
 
-# Erases everything from the cursor to the end of the screen.
-def erase():
-    print("\x1B[0J", end = "")
+def erase(): # Erases everything from the cursor to the end of the screen.
+    print("\x1B[0J", end = "") # clears terminal screen
